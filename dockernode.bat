b0e5aed2fc0f9b6b415b8a5ef55294e0c2488170
Francesco Delfino <delfinof@gmail.com>
2016-06-22 09:35:42 +0200

    Made the dockernode file general

--- dockernode.bat
+++ dockernode.bat
@@ -1,5 +1,2 @@
 
-
-
-
-docker run -it -v c:/Programming/debugging-in-docker:/debugging-in-docker -e PROXY_PORT=8181 -p 8181:8181 -p 5858:5858 mhart/alpine-node:4.4.5 node %*
+docker run -it --workdir=/mnt -v %~dp0:/mnt -e PROXY_PORT=8181 -p 8181:8181 -p 5858:5858 mhart/alpine-node:4.4.5 node %*
